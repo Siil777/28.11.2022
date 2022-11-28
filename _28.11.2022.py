e55fe3c3600f39a8aba9196d3e6a609f504046e3
opilane <opilane@HP-CZC6297B9M>
2022-11-28 16:14:54 +0200

    Add project files.

---
+++ _28.11.2022.py
@@ -0,0 +1,82 @@
+﻿#arv=print("arv")
+
+#try:
+#    arv=int(arv)
+#    print("int")
+#except:
+#    try:
+#        arv=float(arv)
+#        print("Float")
+#    except:
+#        print("tekst")
+
+
+#Задача 
+
+#В программе должны выполняться ниже следующие действия:
+
+#            вывод на экран приветствия "Tere! Olen sinu uus sõber - Python!"
+#            присваивание переменной nimi введённого пользователем имени
+#            вывод на экран текста: nimi ", oi kui ilus nimi!" 
+#            вывод на экран текста: nimi  "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => "
+
+#print("Tere! Olen sinu uus sober - Python")
+
+#nimi=input("")
+#print(",oi kui ilus nimi!", nimi)
+#vastus=input("Kas leian Sinu keha indeksi ? 0-ei, ja-jah=>")
+#a=input("Pikkus")
+#print("Pikkus")
+#if a.isdigit():
+
+
+print("Tere! Olen sinu uus sober - Python")
+a=input("Kas leian Sinu keha indeksi ? 0-ei, 1-jah=>")
+if a=="1":
+    try:
+        pikkus=int(input("pikkus"))
+    except:
+        pikkus=175
+        print("error, pikkus=175")
+    try:
+        mass=int(input("Mass: "))
+    except:
+        mass=55
+        print("Error, siis mass=55")
+    try:
+        index=mass/0.01**2
+        if index<16:
+            print("Ohtlik alakaal")
+        elif index>=16 and index<19:
+            print("Alakaal")
+    except:
+        print("error")
+    if index>15 and index<20:
+        print("Alakaal")
+    if index>18 and index<26:
+        print("Normaalkaal")
+    if index>24 and index<31:
+        print("Ulekaal")
+    if index>29 and index<36:
+        print("Rasvumine")
+    if index>34 and index<41:
+        print("Tugev rasvumine")
+    if index>40:
+        print("Ohtlik rasvumine")
+  
+    
+
+
+
+
+
+
+
+        
+ 
+
+    
+
+
+
+
